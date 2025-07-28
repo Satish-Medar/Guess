1e55abd924953facf2fdb6ef0325ea5969d3c699
{Satish11222} <satishmedar5@gmail.com>
2025-07-28 13:57:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,149 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const { v4: uuidv4 } = require("uuid");
+const port = process.env.PORT || 3000;
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Server has started ${port}`);
+});
+
+app.get("/game/postgame", (req, res) => {
+  res.render("post.ejs");
+});
+
+let data = [
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 1,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 2,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 3,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 4,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 5,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 6,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 7,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 8,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 9,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 10,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 11,
+    mon: "Rupees: ₹50",
+  },
+  {
+    id: uuidv4(),
+    Username: "satishmedar",
+    p: 12,
+    mon: "Rupees: ₹50",
+  },
+];
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/game", (req, res) => {
+  res.render("index.ejs", { data });
+});
+
+app.post("/game", (req, res) => {
+  let id = uuidv4();
+  let { name, number, Username, money } = req.body;
+  let mon = `Rupees: ₹${money}`;
+  let p = number;
+  data.push({ id, number, Username, p, mon });
+  console.log(mon);
+  res.redirect("/game");
+});
+
+app.get("/game/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let person = data.find((p) => id == p.id);
+  if (person == undefined) {
+    res.render("error.ejs");
+  } else {
+    res.render("edit.ejs", { person });
+  }
+});
+
+app.patch("/game/:id", (req, res) => {
+  let { id } = req.params;
+  let person = data.find((p) => id == p.id);
+  let newUsername = req.body.Username;
+  person.Username = newUsername;
+  res.redirect("/game");
+});
+
+let number = 99;
+app.delete("/game/:id", (req, res) => {
+  let { id } = req.params;
+  data = data.filter((p) => id != p.id);
+  res.redirect("/game");
+});
+
+let winningNumber = 99; // default value
+
+app.post("/set-number", (req, res) => {
+  const { winningNumber: newNumber } = req.body;
+  winningNumber = newNumber;
+  res.status(200).json({ message: "Number updated" });
+});
+app.delete("/game", (req, res) => {
+  res.render("winners.ejs", { data, winningNumber });
+});
---
+++ public/app.js
@@ -0,0 +1,87 @@
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btns = document.querySelectorAll(".time_btns");
+let time_selected;
+console.log(btns);
+let num = null;
+btn1.addEventListener("click", () => {
+  time_selected = btn1.id;
+  timer(time_selected);
+  for (let btn of btns) {
+    btn.classList.add("disabled");
+  }
+});
+btn2.addEventListener("click", () => {
+  time_selected = btn2.id;
+  timer(time_selected);
+  for (let btn of btns) {
+    btn.classList.add("disabled");
+  }
+});
+btn3.addEventListener("click", () => {
+  time_selected = btn3.id;
+  timer(time_selected);
+  for (let btn of btns) {
+    btn.classList.add("disabled");
+  }
+});
+btn4.addEventListener("click", () => {
+  time_selected = btn4.id;
+  timer(time_selected);
+  for (let btn of btns) {
+    btn.classList.add("disabled");
+  }
+});
+
+function timer(time_selected) {
+  let timeLeft = time_selected * 60; // change to 5 * 60 for 5 minutes
+  console.log(timeLeft);
+  let time = timeLeft * 1000;
+  const timerDisplay = document.getElementById("timer");
+
+  const countdown = setInterval(() => {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+
+    // Add leading zero to seconds if needed
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    timerDisplay.textContent = `${minutes}:${seconds}`;
+
+    timeLeft--;
+
+    if (timeLeft < 0) {
+      clearInterval(countdown);
+      timerDisplay.textContent = "10:00";
+    }
+  }, 1000);
+
+  setTimeout(() => {
+    number();
+    for (let btn of btns) {
+      btn.classList.remove("disabled");
+    }
+  }, time + 1000);
+}
+
+function number() {
+  let number = Math.floor(Math.random() * 99);
+  let rand_number = document.getElementById("number");
+
+  // Display it in the UI
+  setTimeout(() => {
+    rand_number.innerHTML = `<h5>Number is: XX</h5>`;
+  }, 3000);
+  rand_number.innerHTML = `<h5>Number is: ${number}</h5>`;
+
+  // Send it to backend
+  fetch("/set-number", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ winningNumber: number }),
+  });
+}
